861ce8412a0aa9994745e7a3cacf3b70dcc385a5
Alex Gochenour <alexgochenour@gmail.com>
2022-05-27 23:58:54 -0400

    test: Error handling

--- lib/convert_string_to_number.ex
+++ lib/convert_string_to_number.ex
@@ -1,5 +1,8 @@
 defmodule Numerify do
   def string_to_number(numberAsString) do
-    String.to_integer(numberAsString)
+    case Integer.parse(numberAsString) do
+      {int, _} -> int
+      :error -> "Please enter an integer as a string."
+    end
   end
 end
--- test/convert_string_to_number_test.exs
+++ test/convert_string_to_number_test.exs
@@ -1,11 +1,16 @@
-_ = "https://www.codewars.com/kata/544675c6f971f7399a000e79"
+_source = "https://www.codewars.com/kata/544675c6f971f7399a000e79"
 
 defmodule NumerifyTest do
   use ExUnit.Case
   import Numerify
 
-  test "It converts a string to a number" do
+  test "It converts a string to a number." do
     result = string_to_number("123")
     assert result == 123
-  end  
+  end
+
+  test "It throws an error when given an empty string." do
+    result = string_to_number("")
+    assert result == "Please enter an integer as a string."
+  end
 end
