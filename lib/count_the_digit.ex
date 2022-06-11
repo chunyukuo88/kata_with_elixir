971a4e3de22c0b5f08853051024de6550d541a02
Alex Gochenour <alexgochenour@gmail.com>
2022-06-11 12:29:48 -0400

    refactor: count_the_digit

--- lib/count_the_digit.ex
+++ lib/count_the_digit.ex
@@ -1,15 +1,15 @@
 defmodule Countdigit do
   def nb_dig(n, d) do
-    digit_as_string = Integer.to_string(d)
     numbers = for int <- 0..n, do: Kernel.trunc(:math.pow(int, 2))
     numbers
-    |> Enum.map(fn(num) -> count_digit_occurrences(num, digit_as_string) end)
+    |> Enum.map(fn(num) -> count_digit_occurrences(num, Integer.to_string(d)) end)
     |> Enum.sum
     end
 
   def count_digit_occurrences(num, digit_as_string) do
-    num_as_string = Integer.to_string(num)
-    num_as_list = String.graphemes(num_as_string)
-    Enum.count(num_as_list, fn(num_digit) -> num_digit == digit_as_string end)
+    num
+    |> Integer.to_string
+    |> String.graphemes
+    |> Enum.count(fn(num_digit) -> num_digit == digit_as_string end)
   end
 end
