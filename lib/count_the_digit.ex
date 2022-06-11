397812c1b22e10b33accb3893b018679b9ed348a
chunyukuo88 <alexgochenour@gmail.com>
2022-06-11 10:23:52 -0400

    switching comps

--- lib/count_the_digit.ex
+++ lib/count_the_digit.ex
@@ -2,8 +2,16 @@ defmodule Countdigit do
 
   def nb_dig(n, d) do
     for int <- 0..n, do: :math.pow(int, 2)
-    |> IO.puts
-    end
-  
+    |> Enum.filter(numbers, fn num -> contains_digit(n, d)
+    |> 
+    )
+  end
+
+  def contains_digit(num, digit) do
+    digit_as_string = Integer.to_string(digit)
+    num_as_string = Integer.to_string(num)
+    num_as_list = String.graphemes(num_as_string)
+    Enum.member?(num_as_list, digit_as_string)
+  end
 end
 
