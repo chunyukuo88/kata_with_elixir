355ee52b2d761b5391e5bffb8aed522d4fd49652
Alex Gochenour <alexgochenour@gmail.com>
2022-06-15 23:19:37 -0400

    added optimal solution from Codewars.

--- lib/word_spinner.ex
+++ lib/word_spinner.ex
@@ -1,5 +1,6 @@
 #https://www.codewars.com/kata/5264d2b162488dc400000001/train/elixir
 
+######### My best solution: #########
 defmodule Spin do
   def spin_words(message) do
     message |> String.split
@@ -11,3 +12,13 @@ defmodule Spin do
     if String.length(word) >= 5, do: String.reverse(word), else: word
   end
 end
+
+######### My favorite  solution from Codewars: #########
+
+#defmodule Spin do
+#  def spin_words(message) do
+#    message
+#    |> String.split(" ")
+#    |> Enum.map_join(" ", &(String.length(&1) >= 5 && String.reverse(&1) || &1))
+#  end
+#end
